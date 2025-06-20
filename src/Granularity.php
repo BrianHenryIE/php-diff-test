f32200f04a616c84dcfa7d0bfbc63ba9174a7cf2
Brian Henry <BrianHenryIE@gmail.com>
2025-06-20 14:42:33 -0700

    Add comment

--- src/Granularity.php
+++ src/Granularity.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Used as an option for the list of tests to return in the filter – only those that match the changed lines
+ * specifically, or all tests that cover a file that has changed.
+ */
+
 namespace BrianHenryIE\PhpDiffTest;
 
 enum Granularity: string
